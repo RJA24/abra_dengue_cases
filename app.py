f804c82db1e78542581573ab44d8d30166cb9857
JangTV <ronjay.1204@gmail.com>
2026-08-20 18:18:01 +0800

    Update button emoji for improved user experience

--- app.py
+++ app.py
@@ -504,12 +504,12 @@ def render_main_menu():
             
     with col2:
         st.markdown('<span class="big-btn-marker"></span>', unsafe_allow_html=True)
-        if st.button("Next Program Sir 😁", use_container_width=True):
+        if st.button("Next Program Sir 😊", use_container_width=True):
             pass
             
     with col3:
         st.markdown('<span class="big-btn-marker"></span>', unsafe_allow_html=True)
-        if st.button("Next Program Sir 😁", use_container_width=True):
+        if st.button("Next Program Sir 😊", use_container_width=True):
             pass
 
 def render_dengue():
