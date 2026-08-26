e30aded65ddb31b457fd971548c68a7bb4b3c2db
JangTV <ronjay.1204@gmail.com>
2026-08-26 09:38:14 +0800

    Update button text in main menu for clarity

--- app.py
+++ app.py
@@ -593,7 +593,7 @@ def render_main_menu():
             
     with col3:
         st.markdown('<span class="big-btn-marker"></span>', unsafe_allow_html=True)
-        if st.button("Next Program Sir  😊", use_container_width=True):
+        if st.button("Nothing to see here now.", use_container_width=True):
             pass
 
 def render_dengue():
