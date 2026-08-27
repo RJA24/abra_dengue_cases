b2af48afa3425fa7d4097714948af041021da0a4
JangTV <ronjay.1204@gmail.com>
2026-08-27 14:49:58 +0800

    Adjust latitude and longitude nudges for Bucloc municipality to improve label positioning

--- app.py
+++ app.py
@@ -336,7 +336,8 @@ def apply_label_nudges(muni_name, lat, lon):
     elif "BUCAY" in name:
         pass
     elif "BUCLOC" in name:
-        pass
+        lat += 0.015
+        lon += 0.015
     elif "DAGUIOMAN" in name:
         pass
     elif "DANGLAS" in name:
